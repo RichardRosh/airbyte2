f87006131475a3f0d2ba7cdefc9c442d2b6f2e01
Richard <84899850+RichardRosh@users.noreply.github.com>
2022-11-15 23:44:07 +0530

    Update test1.sql

--- models/examples/test1.sql
+++ models/examples/test1.sql
@@ -4,9 +4,9 @@ materialized = "table"
 )
 }}
 
-with customers as (
+with testtable as (
     
     SELECT id as Health_Service_Area,Hospital_Counrt,Zip_Code,CONCAT(Health_Service_Area, ' ', Zip_Code) Service_Area_Code FROM testtable
 )
 
-select * from testable
+select * from testtable
